e78202e5e873377901e1a12a46248ad4b8bf0f0a
Secret-ForYou <unbanwa103@gmail.com>
2024-11-12 01:59:30 -0800

    Update script.js

--- script.js
+++ script.js
@@ -7,9 +7,3 @@ document.getElementById("changeMessageBtn").addEventListener("click", function()
 document.getElementById("goToSecondPageBtn").addEventListener("click", function() {
     window.location.href = "index2.html"; // Mengarah ke index2.html
 });
-
-// Fungsi untuk memulai audio saat halaman dimuat
-window.addEventListener("load", function() {
-    var audio = document.getElementById("myAudio");
-    audio.play(); // Memulai audio secara otomatis
-});
